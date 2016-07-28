47761d99cd5ca073c08d7c82d58122185eb80b97
Altran <Altran@10.12.32.72>
2016-07-28 15:50:48 +0100

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml
    	src/conf/MANIFEST.MF

--- src/java/Beans/ManagePergunta.java
+++ src/java/Beans/ManagePergunta.java
@@ -24,8 +24,7 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import org.primefaces.context.RequestContext;
-import org.primefaces.event.SelectEvent;
+
 
 /**
  *
